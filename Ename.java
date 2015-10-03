283f01dc8bf82d38b7c7b278a78a989cd13a43cc
olethka5 <olethka5@gmail.com>
2015-10-04 00:32:19 +0300

    testfile
    testfile

---
+++ Ename.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ename {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String name = in.nextLine();
+		    System.out.println("Привет " + name);
+		in.close();
+
+	}
+
+}
---
+++ Stature.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Stature {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("eenter your height");
+		int a = in.nextInt();
+		System.out.println("enter your weight");
+		int b = in.nextInt();
+			 if ((a-100)<b)
+		  {
+		      System.out.println("you need to lose weight");
+		  }else{
+              System.out.println("you need to put on weight"); }
+			in.close();
+	}
+
+}
