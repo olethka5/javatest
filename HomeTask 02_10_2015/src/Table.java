31de6d62118bc6faf7b8868a8c988cfc11ed3150
olethka5 <olethka5@gmail.com>
2015-10-12 10:20:19 +0300

    HomeTask 02_10_2015

--- HomeTask 02_10_2015/src/Table.java
+++ HomeTask 02_10_2015/src/Table.java
@@ -1,4 +1,3 @@
-import static java.lang.System.out;
 
 public class Table {
 
@@ -7,10 +6,10 @@ public class Table {
 		for (int x = 1; x <= 10; x++) {
 			for (int y = 1; y <= 10; y++) {
 				int result = x * y;
-				out.printf("%4d", result);
+				System.out.printf("%4d", result);
 
 			}
-			out.println();
+			System.out.println();
 		}
 	}
 }
